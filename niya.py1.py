0cc28c379f22741a6275740640a325db9b166019
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-22 15:06:14 +0530

    Add files via upload

---
+++ niya.py1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter a  first number"))
+num2=int(input("Enter a second number"))
+num3=int(input("Enter a third number"))
+large=0
+if num1>num2:
+    large=num1
+elif large>num3:
+    print(num1,"is the largest")
+else:
+    print(num3,"is the largest")
