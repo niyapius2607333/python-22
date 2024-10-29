db996f097f41487f11c75b8452678b7e295d747b
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-29 14:33:48 +0530

    niya.py3.py

--- niya.py3.py
+++ niya.py3.py
@@ -1,3 +1,9 @@
+'''
+Author : Niya Pius
+Date : 15-10-2024
+Python program to find a number is prime or not
+Version 1.0
+'''
 from itertools import filterfalse
 
 check_prime=int(input("Enter a number"))
