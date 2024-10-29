4426cefad46176f748a0030122d08bf670f6d42d
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-29 16:31:42 +0530

    niya.py5.py

--- niya.py5.py
+++ niya.py5.py
@@ -1,3 +1,9 @@
+'''
+Author : Niya Pius
+Date : 15-10-2024
+Python program to print multiplication table of a number
+Version 1.0
+'''
 number=int(input("Enter a number"))
 limit=int(input("Enter the limit"))
 for i in range(0,limit):
